d49517e8cdc5df80293646c40fa1d97010319d20
A. Lopez <140568825+angelllopez@users.noreply.github.com>
2023-08-24 17:06:37 -0500

    Several Changes

---
+++ MySolarPower.Data/Models/Partial Classes/PowerUsageDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySolarPower.Data.Models.Partial_Classes
+{
+    public partial class PowerUsageDbContext : DbContext
+    {
+        public PowerUsageDbContext(DbContextOptions<PowerUsageDbContext> options) : base(options)
+        {
+        }
+
+
+    }
+}
--- mySolarPower.API/Program.cs
+++ mySolarPower.API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using MySolarPower.Data.Contracts;
 using MySolarPower.Data.Models;
 using MySolarPower.Data.Repositories;
@@ -7,9 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<PowerUsageDbContext>();
-builder.Services.AddScoped<IProductionRepository, ProductionRepository>();
 
+builder.Services.AddDbContext<PowerUsageDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IProductionRepository, ProductionRepository>();
+//builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
