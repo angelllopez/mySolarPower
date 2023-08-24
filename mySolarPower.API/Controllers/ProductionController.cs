45a239f026348c647b74381524811319528bdb15
A. Lopez <140568825+angelllopez@users.noreply.github.com>
2023-08-24 00:20:48 -0500

    More Changes.

---
+++ mySolarPower.API/Controllers/ProductionController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySolarPower.Data.Contracts;
+
+namespace mySolarPower.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductionController : ControllerBase
+    {
+        private readonly IProductionRepository _repository;
+
+        public ProductionController(IProductionRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProductionDataAsync()
+        {
+            var productionData = await _repository.GetProductionDataAsync();
+            if (!productionData.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(productionData);
+        }
+    }
+}
