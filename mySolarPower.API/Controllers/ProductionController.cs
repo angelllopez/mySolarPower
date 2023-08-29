5a4ce2e187e33454c4ecc8233c7c76dc910d53cd
A. Lopez <140568825+angelllopez@users.noreply.github.com>
2023-08-29 14:37:48 -0500

    Add XML documentation and configured Swagger to display it.

--- mySolarPower.API/Controllers/ProductionController.cs
+++ mySolarPower.API/Controllers/ProductionController.cs
@@ -3,17 +3,33 @@ using MySolarPower.Data.Contracts;
 
 namespace mySolarPower.API.Controllers
 {
+    /// <summary>
+    /// Controller for production data.
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class ProductionController : ControllerBase
     {
         private readonly IProductionRepository _repository;
 
+        /// <summary>
+        /// Initializes a new instance of the ProductionController class.
+        /// </summary>
+        /// <param name="repository">Production repository interface</param>
         public ProductionController(IProductionRepository repository)
         {
             _repository = repository;
         }
 
+        /// <summary>
+        /// Retrieves production data from a repository and returns it as a response.
+        /// </summary>
+        /// <returns>
+        /// An IActionResult object that contains either a 404 status code
+        /// and a message if the production data is not available, or a 200
+        /// status code and the production data as the content if it is 
+        /// available.
+        /// </returns>
         [HttpGet]
         public async Task<IActionResult> GetProductionDataAsync()
         {
